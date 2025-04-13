314da152f2677c95e4cda45c7de5b7724bf4b9d2
University <University@DESKTOP-N0DCL36>
2025-04-13 20:25:39 +0200

    Add project files.

---
+++ Practical2/Controllers/PeopleController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+using Practical2.Models;
+
+namespace Practical2.Controllers
+{
+    public class PeopleController : Controller
+    {
+        // GET: People
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+        public static List<PersonModel> people = new List<PersonModel>()
+        {
+            new PersonModel{
+                StuNumber = "u0001",
+                FirstName = "Name 1",
+                LastName = "Surname 1",
+                Email = "person1@tuks.co.za",
+
+
+            },
+            new Models.PersonModel
+            {
+                StuNumber = "u0002",
+                FirstName = "Name 2",
+                LastName = "Surname 2",
+                Email = "person2@tuks.co.za",
+
+            },
+            new Models.PersonModel
+            {
+                StuNumber = "u0003",
+                FirstName = "Name 3",
+                LastName = "Surname 3",
+                Email = "person3@tuks.co.za",
+
+            },
+            new Models.PersonModel
+            {
+                StuNumber = "u0004",
+                FirstName = "Name 4",
+                LastName = "Surname 4",
+                Email = "person4@tuks.co.za",
+
+            },
+            new Models.PersonModel
+            {
+                StuNumber = "u0005",
+                FirstName = "Name 5",
+                LastName = "Surname 5",
+                Email = "person5@tuks.co.za",
+
+            }
+        };
+
+
+
+
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        public ActionResult ListPeople()
+            {
+            
+
+                return View(people);
+
+            }
+        }
+
+}
---
+++ Practical2/Models/PersonModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Practical2.Models
+{
+    public class PersonModel
+    {
+        //Quick property - type "prop" press tab x2
+        //Complete the property details
+        //This is the "Model" of "data source"
+        [Display(Name = "Student number")] //Add as decorator
+        public string StuNumber { get; set; }
+
+        [Display(Name = "First Name")] //Add as decorator
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")] //Add as decorator
+        public string LastName { get; set; }
+
+        [Display(Name = "Email address")] //Add as decorator
+        public string Email { get; set; }
+
+        
+    }
+}
